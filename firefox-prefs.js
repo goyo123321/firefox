d13ac9c72492d14102e0d0a77dbfdee6c73067ab
goyo123321a <goyo123321@mail.fresnostate.edu>
2025-12-07 00:27:13 +0800

    Create firefox-prefs.js

---
+++ firefox-prefs.js
@@ -0,0 +1,5 @@
+user_pref("browser.startup.page", 3);
+user_pref("startup.homepage_welcome_url", "");
+user_pref("xpinstall.signatures.required", false);
+user_pref("extensions.experiments.enabled", true);
+user_pref("gfx.webrender.all", false);
